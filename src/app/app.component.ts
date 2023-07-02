5451ae7080618fdc2d50793aefa543f5f981e9ef
Page Dresser <pagedresser@gmail.com>
2023-07-02 11:59:01 +0200

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { trigger, style, animate, transition } from '@angular/animations';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
@@ -12,10 +13,23 @@ import { PostComponent } from './post/post.component';
   imports: [CommonModule, PostComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [loadingInfoAnimation()],
 })
 export class AppComponent {
   posts$: Observable<Post[]> | undefined;
   constructor(private postClient: PostsClient) {
     this.posts$ = this.postClient.list();
   }
+}
+
+function loadingInfoAnimation() {
+  return trigger('loadingInfo', [
+    transition(':leave', [
+      style({
+        opacity: 1,
+        height: '*',
+      }),
+      animate('700ms ease-in-out', style({ opacity: 0, height: 0 })),
+    ]),
+  ]);
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient(),
+  ],
 };
